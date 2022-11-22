0257cb5511aba58cf3f5d053ef2d8e26b3da45d9
João Lucas <Jlucasoc@hotmail.com>
2022-11-22 15:11:12 -0300

    n-ary tree 🎄

--- structures/arvore_naria.c
+++ structures/arvore_naria.c
@@ -1,13 +1,77 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-typedef struct raiz {
+typedef struct no {
     int info;
-    
-} raiz;
+    struct no *filho;
+    struct no *irmao;
+} no;
+
+no *cria_no(int info) {
+    no *novo = (no *) malloc(sizeof(no));
+    novo->info = info;
+    novo->filho = NULL;
+    novo->irmao = NULL;
+    return novo;
+}
+
+no *insere_filho(no *pai, no *filho) {
+    if (pai->filho == NULL) {
+        pai->filho = filho;
+    } else {
+        no *aux = pai->filho;
+        while (aux->irmao != NULL) {
+            aux = aux->irmao;
+        }
+        aux->irmao = filho;
+    }
+    return pai;
+}
+
+int busca(no *pai, int info){
+    if (pai == NULL){
+        return 0;
+    }
+    if (pai->info == info){
+        return 1;
+    }
+    if (busca(pai->filho, info) == 1){
+        return 1;
+    }
+
+    return busca(pai->irmao, info);
+}
+
+void imprimir_arvore(no *pai) {
+    if (pai == NULL) {
+        return;
+    }
+    printf("%d ", pai->info);
+    imprimir_arvore(pai->filho);
+    imprimir_arvore(pai->irmao);
+}
 
 int main(){
 
-    raiz *arvore = Criar();
+    no *raiz = cria_no(1);
+    no *filho1 = cria_no(2);
+    no *filho2 = cria_no(3);
+    no *filho3 = cria_no(4);
+
+    insere_filho(raiz, filho1);
+    insere_filho(raiz, filho2);
+    insere_filho(raiz, filho3);
+
+    printf("\n");
+    imprimir_arvore(raiz);
+    printf("\n");
+
+    if (busca(raiz, 4) == 1){
+        printf("Achou");
+    } else {
+        printf("Nao achou");
+    }
+
 
     return 0;
 }
