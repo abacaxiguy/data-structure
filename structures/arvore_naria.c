034f24c9a411e175a355f540461bf06496d11b7e
João Lucas <Jlucasoc@hotmail.com>
2022-11-24 22:39:49 -0300

    Union with n-ary tree 🎄♾

--- structures/arvore_naria.c
+++ structures/arvore_naria.c
@@ -51,6 +51,55 @@ void imprimir_arvore(no *pai) {
     imprimir_arvore(pai->irmao);
 }
 
+no *copiar(no *arvore1, no *arvore2)
+{
+    if (arvore1 == NULL)
+    {
+        return arvore2;
+    }
+
+    no *novo = cria_no(arvore1->info);
+
+    arvore2 = insere_filho(arvore2, novo);
+
+    arvore2 = copiar(arvore1->irmao, arvore2);
+
+    arvore2 = copiar(arvore1->filho, arvore2);
+
+    return arvore2;
+}
+
+no *unir(no *arvore1, no *arvore2)
+{
+    if (arvore1 == NULL)
+    {
+        return arvore2;
+    }
+
+    if (busca(arvore2, arvore1->info) == 0)
+    {
+        no *novo = cria_no(arvore1->info);
+        arvore2 = insere_filho(arvore2, novo);
+    }
+
+    arvore2 = unir(arvore1->irmao, arvore2);
+
+    arvore2 = unir(arvore1->filho, arvore2);
+
+    return arvore2;
+}
+
+no *uniao(no *arvore1, no *arvore2){
+    no *arvore3 = cria_no(arvore1->info);
+
+    arvore3 = copiar(arvore1->filho, arvore3);
+
+    arvore3 = unir(arvore2, arvore3);
+
+    return arvore3;
+}
+
+
 int main(){
 
     no *raiz = cria_no(1);
@@ -62,16 +111,49 @@ int main(){
     insere_filho(raiz, filho2);
     insere_filho(raiz, filho3);
 
-    printf("\n");
+    insere_filho(filho1, cria_no(5));
+    insere_filho(filho1, cria_no(6));
+    insere_filho(filho2, cria_no(7));
+
+    printf("Arvore 1:\n");
     imprimir_arvore(raiz);
     printf("\n");
 
+    printf("\nBuscando na árvore 1 o número 4:");
     if (busca(raiz, 4) == 1){
-        printf("Achou");
+        printf("\nAchou o número 4!\n\n");
     } else {
-        printf("Nao achou");
+        printf("\nNao achou o número 4!\n\n");
     }
 
+    no *raiz2 = cria_no(1);
+    no *filho4 = cria_no(7);
+    no *filho5 = cria_no(8);
+    no *filho6 = cria_no(9);
+
+    insere_filho(raiz2, filho4);
+    insere_filho(raiz2, filho5);
+    insere_filho(raiz2, filho6);
+
+    printf("Arvore 2:\n");
+    imprimir_arvore(raiz2);
+    printf("\n");
+
+    no *raiz3 = uniao(raiz, raiz2);
+
+    printf("\nUnindo as duas árvores:\n");
+    printf("\nArvore 3:\n");
+    imprimir_arvore(raiz3);
+    printf("\n");
+
+    printf("\nChecando se nenhuma árvore foi alterada:\n");
+    printf("\nArvore 1:\n");
+    imprimir_arvore(raiz);
+    printf("\n");
+
+    printf("\nArvore 2:\n");
+    imprimir_arvore(raiz2);
+    printf("\n");
 
     return 0;
 }
