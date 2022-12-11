dea8000155d01f026e030fa55d681b81f6f0ac24
João Lucas <Jlucasoc@hotmail.com>
2022-12-11 13:18:10 -0300

    Redoing depth-first search ♻

--- graphs/busca_profundidade.c
+++ graphs/busca_profundidade.c
@@ -11,22 +11,3 @@
 //  | /
 //  C
 
-int main(){
-    graph *g = create_graph('A');
-    add_neighbor(g, 'B');
-    add_neighbor(g, 'C');
-
-    graph *g2 = create_graph('B');
-    add_neighbor(g2, 'A');
-    add_neighbor(g2, 'C');
-
-    graph *g3 = create_graph('C');
-    add_neighbor(g3, 'A');
-    add_neighbor(g3, 'B');
-
-    print_graph(g);
-    print_graph(g2);
-    print_graph(g3);
-
-    return 0;
-}
